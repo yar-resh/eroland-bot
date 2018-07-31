45d3565014138a7446f49dcad4d69e3ecc752a2f
Yaroslav R <reshetnik.yaroslav@gmail.com>
2018-07-31 21:29:38 +0300

    Reduced get_random_images for ErolubProvider.

--- providers/__init__.py
+++ providers/__init__.py
@@ -91,28 +91,18 @@ class ErolubProvider(EroBaseProvider):
         :return list: list with urls of random images.
         """
         result_images_urls = []
-        pages_amount = self.pages_amount
-        random_page_numbers = get_random_page_numbers(amount, pages_amount)
-
-        for number in random_page_numbers:
+        for number in get_random_page_numbers(amount, self.pages_amount):
             time.sleep(1)
-            try:
-                posts = self._get_posts_on_page(number)
-                random_post = random.choice(posts)
-                post_url = random_post.find('a', recursive=True)['href']
-                print('Getting post content from: ' + post_url)
-                response = self._session.get(url=post_url)
-                bs = bs4.BeautifulSoup(response.text)
-            except:
-                continue
-
+            random_post = random.choice(self._get_posts_on_page(number))
+            post_url = random_post.find('a', recursive=True)['href']
+            print('Getting post content from: ' + post_url)
+            response = self._session.get(url=post_url)
+            bs = bs4.BeautifulSoup(response.text)
             gallery = bs.find('div', class_='pw-description clearfix')
             if gallery is not None:
-                random_image = random.choice(gallery.find_all('img'))
-                url = random_image['src']
-                print('Image url: ' + url)
-                result_images_urls.append('http://erolub.com/' + url)
-
+                random_url = random.choice(gallery.find_all('img'))['src']
+                print('Image url: ' + random_url)
+                result_images_urls.append('http://erolub.com/' + random_url)
         return result_images_urls
 
 
