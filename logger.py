b6dbbe7f588c1448cfaf04536d8182334f75908e
Yaroslav R <reshetnik.yaroslav@gmail.com>
2018-08-02 00:09:40 +0300

    Changed logging format.

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ from logging import getLogger, DEBUG, Formatter, StreamHandler
 
 logger = getLogger('itd-eroland-bot')
 logger.setLevel(DEBUG)
-logger_format = Formatter('%(asctime)s - %(levelname)s - %(lineno)d - %(message)s')
+logger_format = Formatter('%(asctime)s - %(levelname)s - %(message)s')
 console_handler = StreamHandler(stdout)
 console_handler.setFormatter(logger_format)
 logger.addHandler(console_handler)
